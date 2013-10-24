39eee4bf94945998e57a7946b6dd67ea7a684fa7
Randy Michael Ledbetter <contact@randyledbetter.com>
2013-10-24 16:13:51 -0700

    Added sprite animation functionality

--- public/js/models/Player.js
+++ public/js/models/Player.js
@@ -16,69 +16,18 @@ var Player = Backbone.Model.extend({
         dispX: 0,
         dispY: 0,
         level: 1,
-        img: '',
+        player: '',
         weapon: '',
         items: [],
-        width: 32,
-        height: 32
+        width: 64,
+        height: 96
     },
 
 
     initialize: function(){
 
-        // Event listeners for changes in key values associated with the Player.
-        // The functions below send feedback to the console when certain
-        // properties change value. These event listeners would not be included
-        // in the production code.
-        console.log(this.get("username") + ", the " + this.get("role") + ", has entered the game.");
-
-        this.on("change:ac", function(model){
-            var ac = model.get("ac");
-            console.log("The " + this.get("role") + "'s Armour Class is now " + parseInt(this.get("ac")) + ".");
-        });
-
-        this.on("change:str", function(model){
-            var str = model.get("str");
-            console.log("The " + this.get("role") + "'s Strength is now " + parseInt(this.get("str")) + ".");
-        });
-
-        this.on("change:con", function(model){
-            var con = model.get("con");
-            console.log("The " + this.get("role") + "'s Constitution is now " + parseInt(this.get("con")) + ".");
-        });
-
-        this.on("change:dex", function(model){
-            var dex = model.get("dex");
-            console.log("The " + this.get("role") + "'s Dexterity is now " + parseInt(this.get("dex")) + ".");
-        });
-
-        this.on("change:speed", function(model){
-            var speed = model.get("speed");
-            console.log("The " + this.get("role") + "'s Speed is now " + parseInt(this.get("speed")) + ".");
-        });
-
-        this.on("change:weapon", function(model){
-            var weapon = model.get("weapon");
-            console.log("The " + this.get("role") + "'s weapon is a " + this.get("weapon") + ".");
-        });
-
-        this.on("change:items", function(model){
-            var items = model.get("items");
-            console.log("The " + this.get("role") + "'s items array includes" + this.get("items") + ".");
-        });
-        this.on("change:x", function(model){
-            var x = model.get("x");
-            console.log("x = " + this.get("x") + ".");
-        });
-        this.on("change:y", function(model){
-            var y = model.get("y");
-            console.log("y = " + this.get("y") + ".");
-        });
-
-        var imageObj = new Image();
-
-        // Add any functions you want to call when a Player object is
-        // first instantiated...
+        
+        // Initialization functions and tasks will go here.
 
     },
 
@@ -87,24 +36,33 @@ var Player = Backbone.Model.extend({
     update: function(keys) {
         // Previous position
         var prevX = this.get("x"),
-            prevY = this.get("y"),
-            imgSize = 64,
-            worldWidth = 160*32,
-            worldHeight = 160*32;
+            prevY = this.get("y");
 
 
 
         if (keys.up) {
             this.set({y: this.get("y") - this.get("speed")});
-        } else if (keys.down) {
+            this.get('player').currentState = 'up';
+            this.get('player').y = (((this.get('y') + 48)/2)-48) - this.get('speed');
+            updateAnimation(this.get('player').stateAnimations[this.get('player').currentState]);
+        } if (keys.down) {
             this.set({y: this.get("y") + this.get("speed")});
+            this.get('player').currentState = 'down';
+            this.get('player').y = (((this.get('y') - 48)/2)-48) + this.get('speed');
+            updateAnimation(this.get('player').stateAnimations[this.get('player').currentState]);
         }
 
 
         if (keys.left) {
             this.set({x: this.get("x") - this.get("speed")});
-        } else if (keys.right) {
+            this.get('player').currentState = 'left';
+            this.get('player').x = (((this.get('x') + 32)/2)-32) - this.get('speed');
+            updateAnimation(this.get('player').stateAnimations[this.get('player').currentState]);
+        } if (keys.right) {
             this.set({x: this.get("x") + this.get("speed")});
+            this.get('player').currentState = 'right';
+            this.get('player').x = (((this.get('x') - 32)/2)-32) + this.get('speed');
+            updateAnimation(this.get('player').stateAnimations[this.get('player').currentState]);
         }
     
     // don't let player leaves the world's boundary
@@ -114,76 +72,83 @@ var Player = Backbone.Model.extend({
         if(this.get("y") - this.get("height")/2 < 0){
             this.set({y: 0 + this.get("height")/2});
         }
-        if(this.get("x") + this.get("width")/2 > worldWidth){
-            this.set({x: worldWidth - this.get("width")/2});
+        if(this.get("x") + this.get("width")/2 > WORLD_WIDTH){
+            this.set({x: WORLD_WIDTH - this.get("width")/2});
         }
-        if(this.get("y") + this.get("height")/2 > worldHeight){
-            this.set({y: worldHeight - this.get("height")/2});
+        if(this.get("y") + this.get("height")/2 > WORLD_HEIGHT){
+            this.set({y: WORLD_HEIGHT - this.get("height")/2});
         }
 
         return (prevX != this.get("x") || prevY != this.get("y")) ? true : false;
 
     },
 
+    makePlayerSprite: function(context) {
+
+        switch(this.get("role"))
+        {
+            case 0:
+                var spriteTiles = new Tileset('images/fighter-sprite-64.png', 64, 96);
+
+                break;
+            case 1:
+                var spriteTiles = new Tileset('images/ranger-sprite-64.png', 64, 96);
+                break;
+            case 2:
+                var spriteTiles = new Tileset('images/mage-sprite-64.png', 64, 96);
+                break;
+            case 3:
+                var spriteTiles = new Tileset('images/cleric-sprite-64.png', 64, 96);
+                break;
+            default:
+                console.log("Failed to assign localPlayer sprite image in localPlayer.initialize()");
+        }
+
+        var spriteLeftAnim = new Animation(spriteTiles, ['3,1', '2,1', '1,1', '0,1'], 200);
+        var spriteRightAnim = new Animation(spriteTiles, ['3,2', '2,2', '1,2', '0,2'], 200);
+        var spriteDownAnim = new Animation(spriteTiles, ['3,0', '2,0', '1,0', '0,0'], 200);
+        var spriteUpAnim = new Animation(spriteTiles, ['0,3', '1,3', '2,3', '3,3'], 200);
+
+        var player = new Sprite({'left': spriteLeftAnim, 'right': spriteRightAnim, 'down': spriteDownAnim, 'up': spriteUpAnim}, 'down', this.get('x'), this.get('y'), 64, 96, 200);
+        this.set('player', player);
+        console.log(player);
+    },
+
     // Draw player
     draw: function(context) 
     {
-        // Fighter = 0, Ranger = 1, Wizard = 2, and Cleric = 3. The user is
-        // prompted upon client connection to select a number between 0 - 3.
-        // This number is assigned to the Player object property 'pClass'
-        // A different color is used to represent each class visually.
+        
         if (this.get("x")<canvas.width/2)
         {
             this.set({dispX: this.get("x")});
         }
-        else if (this.get("x")>(160*32-canvas.width/2))
+        else if (this.get("x")>(WORLD_WIDTH-canvas.width/2))
         {
-            this.set({dispX: this.get("x")-(160*32-canvas.width)});
+            this.set({dispX: this.get("x")-(WORLD_WIDTH-canvas.width)});
         }
         else
         {
             this.set({dispX: canvas.width/2});
         }
-        //console.log(canvas.width/2);
+      
 
         if (this.get("y")<canvas.height/2)
         {
             this.set({dispY: this.get("y")});
         }
-        else if (this.get("y")>(160*32-canvas.height/2))
+        else if (this.get("y")>(WORLD_HEIGHT-canvas.height/2))
         {
-            this.set({dispY: this.get("y")-(160*32-canvas.height)});
+            this.set({dispY: this.get("y")-(WORLD_HEIGHT-canvas.height)});
         }
         else
         {
             this.set({dispY: canvas.height/2});
         }
 
-        switch(this.get("role"))
-        {
-            case 0:
-                var img = ASSET_MANAGER.getAsset('images/fighter.png');
-                context.drawImage(img, this.get("dispX") - img.width/2, this.get("dispY") - img.height/2, 64, 64);
-                //console.log('fighter drawImage was processed.');
-                break;
-            case 1:
-                var img = ASSET_MANAGER.getAsset('images/archer.png');
-                context.drawImage(img, this.get("dispX") - img.width/2, this.get("dispY") - img.height/2, 64, 64);
-                //console.log('archer drawImage was processed.');
-                break;
-            case 2:
-                var img = ASSET_MANAGER.getAsset('images/wizard.png');
-                context.drawImage(img, this.get("dispX") - img.width/2, this.get("dispY") - img.height/2, 64, 64);
-                //console.log('wizard drawImage was processed.');
-                break;
-            case 3:
-                var img = ASSET_MANAGER.getAsset('images/cleric.png');
-                context.drawImage(img, this.get("dispX") - img.width/2, this.get("dispY") - img.height/2, 64, 64);
-                //console.log('cleric drawImage was processed.');
-                break;
-            default:
-                console.log("The role property passed to the Player.draw() function is greater than 3. Fix it.");
-        }
+
+
+        drawSprite(this.get('player'));
+               
     }
 
     
